4786d8d7128f50e36225da6801e69346a592b2c4
Parth Joshi <parth.00.joshi@gmail.com>
2026-08-07 14:11:32 +0530

    Next16: Added sample data, product type, product list and product card based on ShadCN Card.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,11 @@
+import ProductList from '@/components/shared/products/product-list';
+import sampleData from '@/db/sample-data';
+
 const Homepage = () => {
 	return (
 		<>
 			<h1>Homepage</h1>
+			<ProductList data={sampleData.products} title='Featured Products' />
 		</>
 	);
 };
