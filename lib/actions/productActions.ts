7896b8b6c39b8101d4adaecbf10af340d4d937ae
Parth Joshi <parth.00.joshi@gmail.com>
2026-08-09 19:56:40 +0530

    Next16: Neon + Prisma serverless using websockets config added. Code refactoring.

--- lib/actions/productActions.ts
+++ lib/actions/productActions.ts
@@ -1,15 +1,10 @@
-import 'dotenv/config';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '../generated/prisma/client';
+import { prisma } from '@/db/prisma';
 import { convertToJsonObject } from '../utils';
 import { LATEST_PRODUCTS_LIMIT } from '../constants';
 // import { Product } from '@/types';
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-
 // get latest products
 export async function getLatestProducts() {
-	const prisma = new PrismaClient({ adapter });
 	try {
 		const products = (
 			await prisma.product.findMany({
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,7 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
-// name conflict handler
+// class name conflict handler
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
